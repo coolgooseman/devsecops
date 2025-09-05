449fa9c198eca1e6c2485e4715427760eb3921ab
Jorn <71011796+coolgooseman@users.noreply.github.com>
2025-09-05 20:34:30 +0200

    Update quoter_templates.py private maken test

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen groetjes jorn!</label>
+<label class="link" for="signinCheckbox">Inloggen groetjes jorn! en private test</label>
 """
 
     return f"""<!DOCTYPE html>
